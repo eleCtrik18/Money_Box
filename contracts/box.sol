457770d93db649d9fc73d767f4a5a40750de769b
Chetan Singh <54989039+eleCtrik18@users.noreply.github.com>
2021-12-15 15:29:20 +0530

    Update box.sol

--- contracts/box.sol
+++ contracts/box.sol
@@ -1,1 +1,19 @@
+pragma solidity 0.8.7;
+
+contract Box{
+    int balance;
+    constructor()public{
+        balance = 0;
+    }
+    function getBalance() view public returns (int){
+        return balance;
+    }
+    function depositBalance(int amt) public {
+        balance = balance + amt;
+    }
+    function withdrawBalance(int amt) public {
+        balance = balance - amt;
+    }
+
+}
 
